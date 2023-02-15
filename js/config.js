198f566d21ba96f33d8796278f4516ca70babbdc
Lone <tbmtbmc@gmail.com>
2023-02-15 17:30:14 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_ZsMrVje1x', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '	19tq5ocb24heocvbq78a2crcp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '19tq5ocb24heocvbq78a2crcp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
